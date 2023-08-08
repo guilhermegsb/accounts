f393bbe3b1fd346299c8363baf6b518063fb4772
Guilherme Milech <guilhermemilech@gmail.com>
2023-08-07 23:26:57 -0300

    arquivos_iniciais

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que deseja fazer?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then()
+    .catch((err) => {
+      console.log(err);
+    });
+}
