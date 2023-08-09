054adb66f58944ad557c2a323b806de9099e5ec0
Guilherme Milech <guilhermemilech@gmail.com>
2023-08-08 23:11:59 -0300

    Criação de contas e sair do aplicativo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+import fs from "fs";
+
 operation();
 
 function operation() {
@@ -19,8 +21,68 @@ function operation() {
         ],
       },
     ])
-    .then()
+    .then((answer) => {
+      const action = answer["action"];
+      if (action === "Criar Conta") createAccount();
+      else if (action === "Sair") sair();
+    })
     .catch((err) => {
       console.log(err);
     });
+}
+
+// criar a conta
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir"));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      { name: "accountName", message: "Digite um nome para a sua conta:" },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      const contasPessoas = `accounts/${accountName}.json`;
+
+      if (fs.existsSync(contasPessoas)) {
+        console.log(
+          chalk.bgRed.black("Esta conta já existe, escolha outro nome!")
+        );
+        buildAccount();
+        return;
+      } else {
+        fs.writeFileSync(
+          contasPessoas,
+          '{"balance":0}',
+          console.log(
+            chalk.green("Parabéns, sua conta foi criada com sucesso!")
+          ),
+          operation(),
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function sair() {
+  console.log(
+    chalk.bgBlue.black("Obrigado por usar o Account,Conta desconectada!")
+  );
+  process.exit();
 }
